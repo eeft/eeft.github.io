3bf8cba83519cb3f2dfaf323fbd4230e15469043
Matteo Stura <MSU@corp.castsoftware.com>
2021-01-14 16:20:34 +0100

    EEFT-0000: fixing boiler plate test

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import bluebird from "bluebird";
 import App from "./app";
 
 const rootElement = document.getElementById("root");
 
-global.Promise = bluebird;
-
-Promise.config({
-  warnings: process.env.NODE_ENV !== "production",
-  cancellation: true,
-});
-
-ReactDOM.render(<App/>,
-  rootElement,
-);
-
-console.log("this works");
+ReactDOM.render(<App/>, rootElement);
